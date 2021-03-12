794a0965d25d513480bfec99ec7ffbf71e083a49
涛声依旧 <45677246+Basicnode@users.noreply.github.com>
2021-03-12 14:52:52 +0800

    Delete venv/forum directory

--- venv/forum/common/boundfunction/bdf.py
+++
@@ -1,6 +0,0 @@
-import re
-# 左右边界函数 (提取字段)
-def findall_data(text, lb="", rb=""):
-    rule = lb + r"(.+?)" + rb
-    datalist = re.findall(rule, text)
-    return datalist
--- venv/forum/main.py
+++
@@ -1,9 +0,0 @@
-from forum.postinvitation.luntan import Dussiz
-
-
-dz = Dussiz()
-dz.DengLu()
-dz.Refresh_Page()
-dz.Upload_file()
-dz.Publise_Invitation()
-dz.Reply_Invitation()
